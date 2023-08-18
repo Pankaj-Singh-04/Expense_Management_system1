b1ac9a3c7e5e1ad60399405afd4b5699170d5152
Pankaj-Singh-04 <114937617+Pankaj-Singh-04@users.noreply.github.com>
2023-08-19 00:41:28 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { loginController, registerController } = require("../controllers/userController");
+const {loginController,registerController}= require ("../controllers/userController");
 
 
 //router object
